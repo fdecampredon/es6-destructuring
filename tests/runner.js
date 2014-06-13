a303bfbc77038c032691fb3c9501b93e1ac47966
François de Campredon <francois.de.campredon@gmail.com>
2014-06-13 08:16:53 +0200

    tests infrastructure

--- tests/runner.js
+++ tests/runner.js
@@ -4,12 +4,13 @@ var fs              = require('fs');
 var path            = require('path');
 var esprima         = require('esprima');
 var recast          = require('recast');
-var recast          = require('recast');
 var destrucuring    = require('../lib/index');
 
 
-var BASELINE = path.join(__dirname, 'tests/baseline');
-var RESULTS  = path.join(__dirname, 'tests/results');
+var CASES   = path.join(__dirname, 'tests/cases');
+var RESULTS = path.join(__dirname, 'tests/results');
+
+var args = process.argv.slice(2);
 
 function transform(filename) {
     var recastOptions = {
@@ -17,16 +18,24 @@ function transform(filename) {
         sourceFileName: filename
     };
     
-    var source = fs.readFileSync(path.join(BASELINE, filename));
+    var source = fs.readFileSync(path.join(CASES, filename));
 
     var ast = recast.parse(source, recastOptions);
     ast = destrucuring.transform(ast);
     var result = recast.print(ast, recastOptions);
 
-    fs.writeFileSync(path.join(RESULTS, filename), result.code, 'utf8');
+    var resultFile = path.join(RESULTS, filename);
+    if(args[0] === '--generate') {
+        fs.writeFileSync(resultFile, result.code, 'utf8');
+    } else {
+        if (fs.readFileSync(resultFile, 'utf8') !== result.code) {
+            console.error('error for test : ' + filename );
+        }
+    }
+    
 }
 
-fs.readdir(BASELINE, function (err, files) {
+fs.readdir(CASES, function (err, files) {
     files.filter(function (fileName) {
         return /.js$/.test(fileName);
     }).forEach(transform);
